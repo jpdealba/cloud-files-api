c8b8a69d20989b174c097db3d1e363743d5f25c7
jpdealba <yoda9@live.com.mx>
2022-11-06 06:10:23 -0600

    CORS OPTIONS

--- index.js
+++ index.js
@@ -7,8 +7,13 @@ const { db, bk } = require("./src/database/database");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 4000;
+const corsOptions = {
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use("/api", apiRoutes);
 
