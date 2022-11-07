95c9fa7980c47c14fc68fc4a6cc8d0017f7d4217
jpdealba <jpdeaty1207@gmail.com>
2022-11-06 19:16:57 -0600

    test

--- src/users/controller.js
+++ src/users/controller.js
@@ -13,8 +13,8 @@ class UsersController {
 
   getList(req, res) {
     const User = new model();
-    res.send(req.body);
-    // User.findFromList(req.body).then((resp) => res.send(resp));
+    // res.send(req.body);
+    User.findFromList(req.body).then((resp) => res.send(resp));
   }
 }
 
