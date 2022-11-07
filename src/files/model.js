a084ec5259db8df86670bebf3a44a4ed929282ff
jpdealba <jpdeaty1207@gmail.com>
2022-11-06 20:31:25 -0600

    53w5

--- src/files/model.js
+++ src/files/model.js
@@ -49,11 +49,15 @@ class File {
   async deleteOne(data) {
     const database = db();
     const filesDb = database.collection("files");
+    // const snapshot = await filesDb
+    //   .where("creator_id", "==", data.creator_id)
+    //   .where("date", "==", data.date)
+    //   .where("file", "==", data.file)
+    //   .where("file_name", "==", data.file_name)
+    //   .get();
     const snapshot = await filesDb
-      .where("creator_id", "==", data.creator_id)
-      .where("date", "==", data.date)
       .where("file", "==", data.file)
-      .where("file_name", "==", data.file_name)
+      .where("creator_id", "==", data.creator_id)
       .get();
 
     // if (snapshot.empty) {
